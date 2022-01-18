c0d12dd0ee3f63d864c8d493d4255e29485024c5
ahmed <ahmedlearn01@gmail.com>
2022-01-17 17:41:29 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folders
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT,()=> console.log(`server is running on port: ${PORT}`));
+// run when client connects
+io.on('connection', socket=>{
+    console.log('new ws connection....');
+
+    socket.emit('msg','hey welcome to the home of developers');
+})
+
+server.listen(PORT,()=> console.log(`server is running on port: ${PORT}`));
