89f7fa4d2706c6248dfe8cc668760861e8e6239d
ahmed <ahmedlearn01@gmail.com>
2022-01-18 07:30:25 -0800

    user identity

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const formatMessages = require('./utils/messages');
+const {userJoin, getCurrentUser} = require('./utils/users');
 
 
 const app = express();
@@ -15,7 +16,6 @@ const PORT = 3000 || process.env.PORT;
 
 // variables
 const botName = 'chatcode';
-const userName = '';
 
 // run when client connects
 io.on('connection', socket=>{
@@ -23,20 +23,28 @@ io.on('connection', socket=>{
     
     // join user
     socket.on('joinRoom', ({username,room})=>{
+        const user = userJoin(socket.id, username,room);
+
+        socket.join(user.room);
+
          // welcome current user
-        socket.emit('msg', formatMessages(botName,'hey welcome to the home of developers'));
+        socket.emit('msg', formatMessages(botName,`hey ${username} welcome to the home of developers`));
 
         // boardcast when a user connects
-        socket.broadcast.emit('msg', formatMessages(botName,'A user has joined the chat'));
+        socket.broadcast.to(user.room).emit('msg', formatMessages(botName,`${username} has joined the chat`));
     })
 
     // listen chat message
     socket.on('chatMessage', msg=>{
-        io.emit('msg', formatMessages('USER', msg))
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('msg', formatMessages(user.username, msg))
     })
      // runs when the client disconnects
      socket.on('disconnect', ()=>{
-        io.emit('msg', formatMessages(botName,'A user has left the chat'));
+         const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('msg', formatMessages(botName,`${user.username} has left the chat`));
     })
 })
 
